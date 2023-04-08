bbc869a69a17c48629a335e4bb76dbdd1659e79f
rakib <rakibsarowar@gmail.com>
2023-04-08 13:44:02 +0600

    useContext

--- src/GrandPa/Grandpa.jsx
+++ src/GrandPa/Grandpa.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import Aunty from '../Aunty/Aunty';
 import Father from '../Father/Father';
 import Uncle from '../Uncle/Uncle';
 import './Grandpa.css'
-
+export const RingCOntext = createContext('Gold Ring')
 const Grandpa = () => {
+    // Step 1: create contex
+     
+    // Step-2: use contex
+
     return (
         <div className='gradpa'>
           <h2>Grandapa</h2>
-          <section className='flex'>
+          {/* Step-2 crate a provider & pass value - in value you can use anything like, functin, array and so on */}
+            <RingCOntext.Provider value='Ring'>
+            <section className='flex'>
             <Father></Father>
             <Uncle></Uncle>
             <Aunty></Aunty>
-
           </section>
+            </RingCOntext.Provider>
         </div>
     );
 };
--- src/Myself/Myself.jsx
+++ src/Myself/Myself.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RingCOntext } from '../GrandPa/Grandpa';
 
 const Myself = () => {
+    const angti = useContext(RingCOntext)
     return (
         <div>
             <h2>Myself</h2>
+            <h2>Gift from Granpa: {angti}</h2>
         </div>
     );
 };
