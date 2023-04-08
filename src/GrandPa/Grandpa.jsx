bea72b4bd2aeb723b0c8bf58d2bc4d0bc01976c8
rakib <rakibsarowar@gmail.com>
2023-04-08 13:52:24 +0600

    added context api

--- src/GrandPa/Grandpa.jsx
+++ src/GrandPa/Grandpa.jsx
@@ -1,25 +1,30 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 import Aunty from '../Aunty/Aunty';
 import Father from '../Father/Father';
 import Uncle from '../Uncle/Uncle';
 import './Grandpa.css'
-export const RingCOntext = createContext('Gold Ring')
+// Step 1: create contex
+export const RingCOntext = createContext('Gold Ring');
+export const MoneyContext = createContext(0);
+
 const Grandpa = () => {
-    // Step 1: create contex
-     
-    // Step-2: use contex
+    const [money, setMoney] = useState(0)
+
 
     return (
         <div className='gradpa'>
-          <h2>Grandapa</h2>
-          {/* Step-2 crate a provider & pass value - in value you can use anything like, functin, array and so on */}
-            <RingCOntext.Provider value='Ring'>
-            <section className='flex'>
-            <Father></Father>
-            <Uncle></Uncle>
-            <Aunty></Aunty>
-          </section>
-            </RingCOntext.Provider>
+            <h2>Grandapa</h2>
+            <p>Has Money: {money}</p>
+            {/* Step-2 crate a provider & pass value - in value you can use anything like, functin, array and so on */}
+            <MoneyContext.Provider value={[money, setMoney]}>
+                <RingCOntext.Provider value='Ring'>
+                    <section className='flex'>
+                        <Father></Father>
+                        <Uncle></Uncle>
+                        <Aunty></Aunty>
+                    </section>
+                </RingCOntext.Provider>
+            </MoneyContext.Provider>
         </div>
     );
 };
--- src/Uncle/Uncle.jsx
+++ src/Uncle/Uncle.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { MoneyContext } from '../GrandPa/Grandpa';
 
 const Uncle = () => {
+    const [money, setMoney] = useContext(MoneyContext)
     return (
         <div>
             <h2>Uncle</h2>
+            <p>Grandpa Money: {money}</p>
+            <button onClick={()=> setMoney(money+1000)}>Sent 1000 taka</button>
         </div>
     );
 };
